594f8ea2e7403481b68eaf6c2a70b25fc027bdce
valyastriz <valyastriz@gmail.com>
2024-05-14 16:14:41 -0400

    added more js to created new elements on blog page and insert them and attached stylings

--- assets/css/js/blog.js
+++ assets/css/js/blog.js
@@ -3,8 +3,18 @@ const formData = JSON.parse(localStorage.getItem('formData'));
 
 const posted = document.createElement('article');
 posted.classList.add('posted');
-posted.innerHTML = 
-    <h2 class="blogTitle">blog post title</h2>
-    <hr class="horisontalLine">
-    <p class="blogContent">blog content</p>
-    <p class="author">author</p>
+
+const title = document.createElement('h2');
+title.textContent= `${formData.title};`
+title.classList.add('blogTitle');
+
+const horisontalLine = document.createElement('hr');
+horisontalLine.classList.add('horisontalLine');
+
+const content = document.createElement('p');
+content.textContent = `${formData.content}`;
+content.classList.add('blogContent');
+
+const author = document.createElement('p');
+author.textContent = `${formData.username}`;
+content.classList.add('author');
